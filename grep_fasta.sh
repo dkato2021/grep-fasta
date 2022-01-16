4e6bc4b4c59b9113a1874006726b94cf4044e66f
dkato <78598272+dkato2021@users.noreply.github.com>
2022-01-16 16:51:33 +0900

    Update grep_fasta.sh

--- grep_fasta.sh
+++ grep_fasta.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#$1:list.txt $2:input.fasta
-seqkit grep -nrf $1 $2 > list.fasta
-seqkit grep -nrvf $1 $2 > unlist.fasta
+#$1:input.fasta $2:list.txt
+seqkit grep -nrf $2 $1 > list.fasta
+seqkit grep -nrvf $2 $1 > unlist.fasta
 seqkit seq -n unlist.fasta > unlist.txt
